335cd8e90e0e0a8c6e44f468c1bd7de56af12b7f
tmsincomb <troysincomb@gmail.com>
2026-02-23 16:16:02 -0800

    local postgres boilerplate

---
+++ sql/local_test_database.sql
@@ -0,0 +1,17 @@
+-- postgres postgres
+-- Local version of test_database.sql for localhost PostgreSQL (collation C)
+-- CONNECT TO postgres USER postgres;
+GRANT "quantdb-test-admin" TO CURRENT_USER;
+
+-- postgres postgres
+
+DROP DATABASE IF EXISTS :test_database;
+
+-- postgres postgres
+
+CREATE DATABASE :test_database -- quantdb
+    WITH OWNER = 'quantdb-test-admin'
+    ENCODING = 'UTF8'
+    CONNECTION LIMIT = -1;
+
+REVOKE "quantdb-test-admin" FROM CURRENT_USER;
